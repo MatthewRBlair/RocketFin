6391f3170339cb8e9d51a461ad8e079dbd740325
Matt Blair <sevenstringblues@gmail.com>
2022-02-15 18:22:08 -0600

    Removed dummy data and added support for config file

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+  "assets/AssetManifest.json": "da1fa5832735714b0e8e6cb0ccb19cea",
+"assets/assets/config.json": "ff322867b74a58e0cec77ee6fb326779",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/NOTICES": "654548a2fed4c106c00bd21de6db19ff",
@@ -17,9 +18,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0919fa3736c085fcb8e16f6bf01c2164",
-"/": "0919fa3736c085fcb8e16f6bf01c2164",
-"main.dart.js": "a1372d7389a7d5ae7f76da6cc5ba6881",
+"index.html": "2f84f7fb5c8692713afdc07256699ca1",
+"/": "2f84f7fb5c8692713afdc07256699ca1",
+"main.dart.js": "2e6bd7fa8517370c3d0b9ec9bce17de4",
 "manifest.json": "cc4840d7f92f687ef66bfb3ecea045e4",
 "version.json": "df0759fb583587cb9a7d94e3a71a8f44"
 };
